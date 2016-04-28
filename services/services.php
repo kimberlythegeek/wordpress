ae5a766539ee588d4e34c1a4d4d3fb5d0e66b00b
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-04-28 13:04:47 -0600

    initial commit

---
+++ services/services.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Serivces CPT
+ * Plugin URI: 
+ * Description: This plugin creates the custom post type Services
+ * Version: 1.0.0
+ * Author: Kimberly Pennington
+ * Author URI: http://acodeabove.com
+ * License: GPL2
+ */
+
+
+?>
