5268bf72425f187a541ad3f99b9128a521367e57
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-05-11 13:15:31 -0600

    changed front-page.php to load content from the Home page instead of hard coded content. changed image urls in sass/variables/_images.scss to ./ relative urls

--- themes/a_code_above/front-page.php
+++ themes/a_code_above/front-page.php
@@ -13,25 +13,16 @@ get_header(); ?>
 
 
 <div id="wrapper">
-  <div class="home-section-1">
-    <div id="graphic" class="section-1"></div>
-    <h1>RESPONSIVE, FLUID DESIGN</h1>
-    <p>These days screens come in all shapes and sizes. We will ensure your website looks just as impressive on every one of them.</p>
-  </div><!-- respond -->
-  <div class="home-section-2">
-    <div id="graphic" class="section-2"></div>
-    <h1>SEARCH ENGINE OPTIMIZATION</h1>
-    <p>An awesome website doesn't count for much if your users can't find it. SEO is essential to optimizing traffic to your site.</p>
-  </div><!-- seo -->
-  <div class="home-section-3">
-    <div id="graphic" class="section-3"></div>
-    <h1>CURRENT CODING STANDARDS</h1>
-    <p>Technology changes quickly. At A Code Above, we make our best effort to be up to date on the latest standards. A cut above the rest!</p>
-  </div><!-- code -->
+<?php
+if ( 'posts' == get_option( 'show_on_front' ) ) {
+    include( get_home_template() );
+} else {
+    include( get_page_template() );
+}
+?>
 </div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
-
 
 
